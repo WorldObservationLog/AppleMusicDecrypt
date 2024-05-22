b894bf5eef3c5aea9373d5f0368e8eb5e5a68b97
WorldObservationLog <wolc@duck.com>
2024-05-22 14:05:11 +0800

    feat: use uvloop or winloop to speed up asyncio

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import asyncio
+import sys
 
 from src.cmd import NewInteractiveShell
 
 if __name__ == '__main__':
+    if sys.platform in ('win32', 'cygwin', 'cli'):
+        import winloop
+        winloop.install()
+    else:
+        import uvloop
+        uvloop.install()
     loop = asyncio.get_event_loop()
     cmd = NewInteractiveShell(loop)
     try:
